7eee339a284703d42dd98369ddf964c485965aa8
Jason Glisson <jason.glisson@gmail.com>
2020-09-15 12:27:07 -0400

    Adding taxonomy script

--- modules/custom/post_update_demo/examples/vocabulary.example.php
+++ modules/custom/post_update_demo/examples/vocabulary.example.php
@@ -1,6 +1,7 @@
 <?php
 
-use Drupal\taxonomy\Entity\Term;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\field\Entity\FieldConfig;
 
 function post_update_demo_post_update_vocabulary() {
 
@@ -13,10 +14,58 @@ function post_update_demo_post_update_vocabulary() {
   if (!isset($vocabularies[$vid])) {
     $vocabulary = \Drupal\taxonomy\Entity\Vocabulary::create(array(
           'vid' => $vid,
-          //'machine_name' => $vid,
           'description' => '',
           'name' => $name,
     ));
+    
+    $fields['category_image'] = [
+      'type' => 'image',
+      'entity_type' => 'user',
+      'bundle' => 'user',
+      'label' => 'Category Picture',
+      'description' => 'Picture for each category.',
+      'required' => FALSE,
+      'widget' => [
+        'type' => 'image_image',
+        'settings' => [
+          'progress_indicator' => 'throbber',
+          'preview_image_style' => 'thumbnail',
+        ],
+      ],
+      'formatter' => [
+        'default' => [
+          'type' => 'image',
+          'label' => 'hidden',
+          'settings' => [
+            'image_style' => 'thumbnail',
+            'image_link' => 'content',
+          ],
+        ],
+      ],
+      'settings' => [
+        'file_extensions' => 'png gif jpg jpeg',
+        'file_directory' => 'images/[date:custom:Y]-[date:custom:m]',
+        'max_filesize' => '',
+        'max_resolution' => '',
+        'alt_field' => FALSE,
+        'title_field' => FALSE,
+        'alt_field_required' => FALSE,
+        'title_field_required' => FALSE,
+      ],
+    ];
+
+    foreach ($fields as $field_name => $config) {
+      $field_storage = FieldStorageConfig::loadByName($config['entity_type'], $field_name);
+      if (empty($field_storage)) {
+        FieldStorageConfig::create(array(
+          'field_name' => $field_name,
+          'entity_type' => $config['entity_type'],
+          'type' => $config['type'],
+        ))->save();
+      }
+    }
+
+
     $vocabulary->save();
 
   } else {
--- modules/custom/post_update_demo/post_update_demo.post_update.php
+++ modules/custom/post_update_demo/post_update_demo.post_update.php
@@ -1,11 +1,14 @@
 <?php
 
+use Drupal\file\Entity\File;
 use Drupal\node\Entity\Node;
+use Drupal\taxonomy\Entity\Term;
 
 function post_update_demo_post_update_demo() {
 
   page_create();
   vocabulary_create();
+  taxonomy_create();
 
 }
 
@@ -44,11 +47,11 @@ function vocabulary_create() {
   
   if (!isset($vocabularies[$vid])) {
     $vocabulary = \Drupal\taxonomy\Entity\Vocabulary::create(array(
-          'vid' => $vid,
-          //'machine_name' => $vid,
-          'description' => '',
-          'name' => $name,
+      'vid' => $vid,
+      'description' => '',
+      'name' => $name,
     ));
+
     $vocabulary->save();
 
   } else {
@@ -60,4 +63,52 @@ function vocabulary_create() {
 
   }
 
+}
+
+function taxonomy_create() {
+
+  \Drupal::logger('post_update_demo')->notice('Adding taxonomy to vocabulary...');
+
+  // function that will process data from the given array
+  function addTaxonomyData($imagePath, $imageNode, $data) {
+    $icon_file = File::create([ 'uri' => $imagePath ]);
+    $icon_file->save();
+    $data[$imageNode]['target_id'] = $icon_file->id();
+    $data[$imageNode]['alt'] = $data['name'];
+
+    Term::create($data)->save();
+  }
+
+  // Set the module and image path
+  $module_path = drupal_get_path('module', 'post_update_demo');
+  $images = $module_path . '/images/';
+
+  // Set vocabulary machine name to use
+  $vid = 'drupal_gov_con';
+
+  // Taxonomy data array
+  $taxonomies = [
+    ['image1.jpg', 'Category1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'],
+    ['image2.jpg', 'Category2', 'Phasellus ac leo ipsum.'],
+    ['image3.jpg', 'Category3', 'Curabitur est nibh, suscipit et laoreet vitae, tristique in purus.']
+  ];
+
+  // loop through the data array and add taxonomies
+  foreach($taxonomies as $key => $array) {
+    addTaxonomyData(
+
+      $images . $array[0],
+      'field_category_image',
+      // pass array that matches the data stucture of the taxonomy term
+      [
+        'parent' => array(),
+        'name' => $array[1],
+        'vid' => $vid,
+        'description' => [
+          'value' => isset($array[2]) ? $array[2] : '',
+          'format' => 'formatted',
+        ]
+      ]
+    ); 
+  }
 }
